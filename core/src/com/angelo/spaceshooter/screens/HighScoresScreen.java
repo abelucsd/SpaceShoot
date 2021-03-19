cbb08f7c6bf57063213751e42eec88bd6684c4e8
Angelo <belmonte.amp@gmail.com>
2021-03-19 12:27:23 -0400

    Working high scores screen

--- core/src/com/angelo/spaceshooter/screens/HighScoresScreen.java
+++ core/src/com/angelo/spaceshooter/screens/HighScoresScreen.java
@@ -2,6 +2,7 @@ package com.angelo.spaceshooter.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.utils.Timer;
@@ -10,14 +11,15 @@ import sun.jvm.hotspot.gc.shared.Space;
 public class HighScoresScreen implements Screen {
     SpaceShooterGame game;
     Texture highScore;
-    private final float START_X = 60;
-    private final float START_Y = 100;
+    private final float START_X = 160;
+    private final float START_Y = 400;
     private final int NUM_SCORES = 5;
     BitmapFont font;
 
     public HighScoresScreen(SpaceShooterGame game){
         this.game = game;
-        // make highscore texture
+        this.highScore = new Texture("highScoresSprite.png");
+        this.font = new BitmapFont();
     }
 
     @Override
@@ -27,16 +29,19 @@ public class HighScoresScreen implements Screen {
 
     @Override
     public void render(float v) {
+        Gdx.gl.glClearColor(0, 0, .2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
         game.batch.draw(this.highScore, START_X, START_Y);
         //font.draw(game.batch, hero.getScore().getScoreString(), 10, 10);
         float x_pos = START_X + 50;
-        float y_pos = START_Y + 30;
+        float y_pos = START_Y - 30;
 
         for (int i = 0; i < NUM_SCORES; i++) {
             font.draw(game.batch, String.valueOf(game.highScores.getScoreList().get(i)), x_pos, y_pos);
-            y_pos += 30;
+            y_pos -= 30;
         }
+
         if (Gdx.input.isTouched()) {
             // touch input button/dimensions
             this.dispose();
--- core/src/com/angelo/spaceshooter/screens/MainMenuScreen.java
+++ core/src/com/angelo/spaceshooter/screens/MainMenuScreen.java
@@ -10,15 +10,19 @@ import com.badlogic.gdx.utils.Timer;
 public class MainMenuScreen implements Screen {
     SpaceShooterGame game;
     Texture mainMenu;
+    Texture highScores;
 
     private final float START_X = 260;
-    private final float START_Y = 100;
+    private final float START_Y = 250;
+    private final float HIGHSCORE_X = 160;
+    private final float HIGHSCORE_Y = 150;
 
     Vector2 touchPosition = new Vector2();
 
     public MainMenuScreen(SpaceShooterGame game) {
         this.game = game;
         this.mainMenu = new Texture("startGameSprite.png");
+        this.highScores = new Texture("highScoresSprite.png");
     }
 
     @Override
@@ -32,15 +36,28 @@ public class MainMenuScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
         game.batch.draw(this.mainMenu, START_X, START_Y);
+        game.batch.draw(this.highScores, HIGHSCORE_X, HIGHSCORE_Y);
 
         if (Gdx.input.isTouched()) {
             touchPosition.set(Gdx.input.getX(), Gdx.input.getY());
-            if (touchPosition.x >= 260 && touchPosition.y >= 300 && touchPosition.x <= 460 && touchPosition.y <= 400) {
+            if (touchPosition.x >= 260 && touchPosition.y >= 170 && touchPosition.x <= 460 && touchPosition.y <= 230) {
                 this.dispose();
                 Timer.schedule(new Timer.Task() {
                     @Override
                     public void run() {
                         game.setScreen(new MainGameScreen(game));
+                    }
+                }, 1);
+            }
+        }
+        if (Gdx.input.isTouched()) {
+            touchPosition.set(Gdx.input.getX(), Gdx.input.getY());
+            if (touchPosition.x >= 160 && touchPosition.y >= 280 && touchPosition.x <= 460 && touchPosition.y <= 350) {
+                this.dispose();
+                Timer.schedule(new Timer.Task() {
+                    @Override
+                    public void run() {
+                        game.setScreen(new HighScoresScreen(game));
                     }
                 }, 1);
             }
